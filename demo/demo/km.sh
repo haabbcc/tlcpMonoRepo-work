ce79c54533dea20cd109b8de348ec39774409043
Electric lunch box <101544952+haabbcc@users.noreply.github.com>
2026-06-11 19:31:21 +0800

    Disable legacy demo helper scripts

--- demo/demo/km.sh
+++ demo/demo/km.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-LIB_DIR=./lib
-INC_DIR=./include
-GM_INC_DIR=./include
+set -e
 
-gcc -ggdb3 -O0 -o server server.c -I${INC_DIR} -I${GM_INC_DIR} -L${LIB_DIR} -lssl -lcrypto -lgm_cipher -ldl -lpthread -Wl,-rpath=${LIB_DIR}
-gcc -ggdb3 -O0 -o client client.c -I${INC_DIR} -I${GM_INC_DIR} -L${LIB_DIR} -lssl -lcrypto -lgm_cipher -ldl -lpthread -Wl,-rpath=${LIB_DIR}
-gcc -ggdb3 -O0 -o server_tls server_tls.c -I${INC_DIR} -I${GM_INC_DIR} -L${LIB_DIR} -lssl -lcrypto -lgm_cipher -ldl -lpthread -Wl,-rpath=${LIB_DIR}
-#gcc -ggdb3 -O0 -o client client.c -I${INC_DIR} -I${GM_INC_DIR} ${LIB_DIR}/libssl.a -L${LIB_DIR} -lcrypto -lSKF_final -lhik_skfm -lgm_cipher -lSecIAC -ldl -lpthread -Wl,-rpath=${LIB_DIR}
+cat >&2 <<'EOF'
+km.sh is a legacy build helper for the old local ./lib and ./include layout.
+It does not build against tlcpMonoRepo/tongsuo and should not be used for the
+current Tongsuo NTLS/TLS demo.
+
+Use:
+
+  cd demo/demo
+  ./mk.sh
+
+mk.sh links against:
+
+  ../../tongsuo
+  ../../tongsuo/include
+EOF
+
+exit 1
